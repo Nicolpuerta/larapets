762869cfb2d359bab24f4e547cde3c25234af924
nicolpuerta <nicoldp.07@gmail.com>
2026-05-07 19:30:46 -0500

    mensaje

---
+++ app/Models/Adoption.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Adoption extends Model
+{
+         /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'pet_id',
+    ];
+
+        //Relationships
+    //Adoption belongs to User
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+            //Relationships
+    //Adoption belongs to Pet
+
+    public function pet(){
+        return $this->belongsTo(Pet::class);
+    }
+}
